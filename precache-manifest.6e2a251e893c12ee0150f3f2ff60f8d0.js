69a32f18ca33ee35df442928716e08ff7e617ee1
崔大师 <1046166@qq.com>
2021-02-03 17:35:28 +0800

    Updates

---
+++ precache-manifest.6e2a251e893c12ee0150f3f2ff60f8d0.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eae0710977e8db2880d439de41c6688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ca6476d52cce2f407a",
+    "url": "/static/css/main.cf602f5d.chunk.css"
+  },
+  {
+    "revision": "4034d802945b518f141d",
+    "url": "/static/js/0.64ef1852.chunk.js"
+  },
+  {
+    "revision": "84608f392220a07194ed",
+    "url": "/static/js/10.60ffb63a.chunk.js"
+  },
+  {
+    "revision": "967912c074fed14cd717",
+    "url": "/static/js/3.77c9dc5a.chunk.js"
+  },
+  {
+    "revision": "35d5344ccada58f363ecb1fca8ade690",
+    "url": "/static/js/3.77c9dc5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2563235ce0726c854e10",
+    "url": "/static/js/4.baf52474.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/4.baf52474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328075e954c26cdc35fb",
+    "url": "/static/js/5.e0859db3.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/5.e0859db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bda2b6166d42bd95f3",
+    "url": "/static/js/6.dc370f29.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/6.dc370f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00907b2030d8b243c6c0",
+    "url": "/static/js/7.e4bd6306.chunk.js"
+  },
+  {
+    "revision": "be2c992b7fa511237dc2",
+    "url": "/static/js/8.446806b7.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.446806b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a401b4b0139e70c0f9f5",
+    "url": "/static/js/9.035311f5.chunk.js"
+  },
+  {
+    "revision": "88ca6476d52cce2f407a",
+    "url": "/static/js/main.baef9d88.chunk.js"
+  },
+  {
+    "revision": "fa47786346df68356a9c",
+    "url": "/static/js/runtime-main.728bbf76.js"
+  },
+  {
+    "revision": "17db7fc28ea8f9cc1b19f24c67b10c60",
+    "url": "/static/media/DAI.17db7fc2.png"
+  },
+  {
+    "revision": "3f620bf12a287bc291b4fe86b5720084",
+    "url": "/static/media/Goswap-logo-GOT.3f620bf1.png"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "/static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "839a0f0d6e0a69fed8d7846344d2100b",
+    "url": "/static/media/USDT.839a0f0d.png"
+  },
+  {
+    "revision": "b735ccc636edd73872e7e79733efaf1f",
+    "url": "/static/media/banks.b735ccc6.png"
+  },
+  {
+    "revision": "9aaf9eb992b0bbdd2baf9310a882133e",
+    "url": "/static/media/goCash (3).9aaf9eb9.png"
+  },
+  {
+    "revision": "211fe839dccc8742afe2d5ac49a77a26",
+    "url": "/static/media/gocash.211fe839.png"
+  },
+  {
+    "revision": "160112f78ee29aa5edfc542db02e21ed",
+    "url": "/static/media/goswap-GOB.160112f7.svg"
+  },
+  {
+    "revision": "73568d603e1f6047fdd47609d75d39d7",
+    "url": "/static/media/goswap-GOC.73568d60.svg"
+  },
+  {
+    "revision": "48ba4fe80133ade79afeea9ac7d9d31a",
+    "url": "/static/media/goswap-GOS.48ba4fe8.svg"
+  },
+  {
+    "revision": "f8fec857dc3b8e244e4c910c550b1df8",
+    "url": "/static/media/sUSD.f8fec857.png"
+  },
+  {
+    "revision": "88ae1ab3f40535ebbd248d03a7aab473",
+    "url": "/static/media/ycrv.88ae1ab3.png"
+  }
+]);
